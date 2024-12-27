cec93437514d4ae42b8d2f731d37bf885fa66c61
leovylle <reabatoctoy@gmail.com>
2024-12-27 05:01:31 -0800

    Add files via upload

---
+++ calendar.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CALENDAR</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<meta name="description" content="School Portal Kit By Toxaswift" />
+		<meta name="keywords" content="School Portal Kit" />
+		<meta name="author" content="Toxaswift" />
+        <link rel="shortcut icon" href="img/icon.png" title="Toxaswift">
+		<link rel="stylesheet" href="css/bootstrap.css" />
+		<link rel="stylesheet" href="css/delily.css" />
+		<link rel="stylesheet" href="css/index.css" />
+		<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/blink.js"></script>
+		<script src="js/index.js"></script>
+		<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+		<script type="text/javascript" src="engine1/jquery.js"></script>
+	</head>
+
+	<body>
+		<div class="container-fluid">
+			<div class="container-fluid" style="border-top-left-radius: 15px; border-top-right-radius: 15px; background: #fff; margin-top: 1%;">
+				<div class="container-fluid" style="padding-bottom: 1.5%;">
+					<div class='col-md-6'>
+						<h1 class='logo'>CTU-TUBURAN CAMPUS</h1>
+					</div>
+					<div class='col-md-2'>
+						&nbsp;
+					</div>
+				</div>
+				
+				<nav class="navbar navbar-inverse custom">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myDropDownMenu" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						
+						<div class="collapse navbar-collapse" id="myDropDownMenu">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+								<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-bullhorn"></span> Notice Board <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="#"><span class="glyphicon glyphicon-picture"></span> School Calendar</a></li>
+									</ul>
+								</li>
+							</ul>
+							<ul class='nav navbar-nav navbar-right'>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span> Portal <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href='login.php'><span class="glyphicon glyphicon-user"></span> Staff Login</a></li>
+										<li><a href='login.php'><span class="glyphicon glyphicon-cog"></span> Student Login</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			<div>
+    <main class="main-grid">
+<img src="img/image.png" alt="school calendar">
+</main>
+            </div>
+      
+    
+   
+			
+			<footer>
+				<div class="row">
+				<div class="container-fluid text-center footer2">
+							<p>Copyright &#169; 2024 All Rights Reserved 
+								&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; CTU TUBURAN PORTAL
+							</p>
+						</div>
+					<div class="col-md-6">
+					</div>
+				</div>
+			</footer>
+		</div>	
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CTU-TC PORTAL</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<meta name="description" content="School Portal Kit By Toxaswift" />
+		<meta name="keywords" content="School Portal Kit" />
+		<meta name="author" content="Toxaswift" />
+        <link rel="shortcut icon" href="img/icon.png" title="Toxaswift">
+		<link rel="stylesheet" href="css/bootstrap.css" />
+		<link rel="stylesheet" href="css/delily.css" />
+		<link rel="stylesheet" href="css/index.css" />
+		<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="js/blink.js"></script>
+		<script src="js/index.js"></script>
+		<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+		<script type="text/javascript" src="engine1/jquery.js"></script>
+	</head>
+
+	<body>
+		<div class="container-fluid">
+			<div class="container-fluid" style="border-top-left-radius: 15px; border-top-right-radius: 15px; background: #fff; margin-top: 1%;">
+				<div class="container-fluid" style="padding-bottom: 1.5%;">
+					<div class='col-md-6'>
+						<h1 class='logo'>CTU-TUBURAN CAMPUS</h1>
+					</div>
+					<div class='col-md-2'>
+						&nbsp;
+					</div>
+				</div>
+				
+				<nav class="navbar navbar-inverse custom">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myDropDownMenu" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						
+						<div class="collapse navbar-collapse" id="myDropDownMenu">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="index.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+								<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-bullhorn"></span> Notice Board <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="calendar.php"><span class="glyphicon glyphicon-picture"></span> School Calendar</a></li>
+									</ul>
+								</li>
+							</ul>
+							<ul class='nav navbar-nav navbar-right'>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-cog"></span> Portal <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href='login.php'><span class="glyphicon glyphicon-user"></span> Staff Login</a></li>
+										<li><a href='login.php'><span class="glyphicon glyphicon-cog"></span> Student Login</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+				
+    <main class="main-grid">
+            <div class="head">
+            <h1 class="page-title">ABOUT US</h1>
+            <p class="subtitle">CTU-TC offers the following curricular programs: Education, Technology, Engineering, Arts and Sciences, and Agriculture</p>
+            </div>
+            <img class="main-image" src="img/ctu-tcadmin.png" alt="ctu-tcadmin">
+            
+            <div class="main-text">
+                <h2 class="section-title">UNIVERSITY PROFILE</h2>
+                <Cebu>As evidenced by the most recent metrics-based ranking release by EduRank, Cebu Technological University is one of the best educational institutions in Cebu, Philippines demonstrating its continued excellence in this area.</Cebu>
+                <p>Eduflank's assessment is based ona thorough analysis that takes into account 23.1k citations obtained from 1.93k scholarly publications at 10 universities in Cebu. </p>
+              
+                <h2 class="section-title sub">CTU RANKING</h2>
+                <th>CTU obtained the 2022 Webometrics Ranking of World Universities, securing the 11th spot.</p>
+                <p>CTU's commendable performance in this arena underscores its commitment to cutting-edge research and its proactive approach to maintaining a robust online presence, facilitating accessible services to stakeholders and constituent alike.</p>
+            </div>
+      
+    </main>
+    
+			
+			<footer>
+				<div class="row">
+				<div class="container-fluid text-center footer2">
+							<p>Copyright &#169; 2024 All Rights Reserved 
+								&nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; CTU TUBURAN PORTAL
+							</p>
+						</div>
+					<div class="col-md-6">
+					</div>
+				</div>
+			</footer>
+		</div>	
+	</body>
+</html>
